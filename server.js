da264d270fb6b0d4e895846371677d576a9423a5
samah-abujwaied <samah.abujweed@yahoo.com>
2021-09-08 16:33:11 +0300

    final edits

--- server.js
+++ server.js
@@ -5,20 +5,20 @@ app.use(express.json());
 const cors = require('cors');
 app.use(cors());
 
-const studentRout = require('./routes/student.rout');
-const supervisorRout = require('./routes/supervisor.rout');
-const errorHandler = require('./error-handlers/500');
-const notFound = require('./error-handlers/404');
-const booksRout = require('./routes/books.rout');
+// const studentRout = require('./routes/student.rout');
+// const supervisorRout = require('./routes/supervisor.rout');
+// const errorHandler = require('./error-handlers/500');
+// const notFound = require('./error-handlers/404');
+// const booksRout = require('./routes/books.rout');
 
 app.get('/', (req, res) => { res.send("hello world");});
 
-app.use(express.urlencoded({ extended: true }));
-app.use(studentRout);
-app.use(supervisorRout);
-app.use(booksRout);
-app.use(errorHandler);
-app.use(notFound);
+// app.use(express.urlencoded({ extended: true }));
+// app.use(studentRout);
+// app.use(supervisorRout);
+// app.use(booksRout);
+// app.use(errorHandler);
+// app.use(notFound);
 
 const start=(port)=>{
     app.listen(port,()=>console.log(`listining to port :  ${port}` ))
